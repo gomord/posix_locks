c492b494464909e5e797e5e32982067f7fc93dee
moti <gomord@gmail.com>
2015-06-19 09:28:51 +0300

    add asm inc

--- lock.c
+++ lock.c
@@ -6,20 +6,29 @@ int get_recoure(){
 	return 1;
 }
 int g_lock = 0;
+
+void asm_inc(int *val){
+	asm("incl %0"::"m"(*val));
+}
+void asm_dec(int *val){
+	asm("decl %0"::"m"(*val));
+}
 void lock(){
 	do{
-		g_lock++;
+		asm_inc(&g_lock);
+		printf("after inc g_lock = %d\n",g_lock);
 		if (g_lock == 1)
 			break;
 		else{
-			g_lock--;
+			asm_dec(&g_lock);
+			printf("g_lock = %d\n",g_lock);
 			sleep(1);
 		}
 	}
 	while(1);
 }
 void unlock(){
-	g_lock--;
+	asm_dec(&g_lock);
 }
 //get resoure with lock
 int main(int argc ,char * argv[]){
