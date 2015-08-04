0eea86d26ea2b19430b5d2d95679f64f68acac3c
moti <gomord@gmail.com>
2015-08-04 15:51:07 +0300

    edit main

--- how_canarys_start/lwn/code/dl/main.c
+++ how_canarys_start/lwn/code/dl/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+/*void *my_memset(void *s, int c, size_t n){
+	volatile void *ts = s;
+	memset(ts,c,n);
+}
+*/
 extern ml_func(int,int);
 int main(){
+	char a[34];
 	printf("hello world %d\n",ml_func(2,3));
+	memset(a,0,34);
 }
--- how_canarys_start/lwn/code/recursion.py
+++ how_canarys_start/lwn/code/recursion.py
@@ -1,4 +1,4 @@
 #! /usr/bin/python
 max_wrapper = 10
 for i in range(1,max_wrapper):
-	open("wrapper%d"%i,'w').write("#! /wrapper%d"%(i-1))
+	open("wrapper%d"%i,'w').write("#! ./wrapper%d -%d\n"%(i-1,i-1))
